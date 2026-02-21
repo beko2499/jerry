9a2eba806dcd542c854490f2280f48b64f8152b8
اسمك <   GitHub>
2026-02-21 22:10:11 +0200

    feat: total wipe script

--- clean_server.sh
+++ clean_server.sh
@@ -1,30 +1,55 @@
 #!/bin/bash
 # clean_server.sh
 # Warning: This script completely formats the existing server for the Jerry Store project.
+# It removes EVERYTHING: Node, PM2, MongoDB, Apache, Nginx, and all project files.
 
-echo ">>> WARNING: THIS SCRIPT WILL WIPE ALL JERRY Store DATA <<<"
-echo ">>> Sleeping for 5 seconds. Press Ctrl+C to cancel. <<<"
-sleep 5
+echo ">>> WARNING: THIS SCRIPT WILL WIPE THE ENTIRE SERVER CLEAN <<<"
+echo ">>> Sleeping for 10 seconds. Press Ctrl+C to cancel. <<<"
+sleep 10
 
 echo "1. Stopping and deleting all PM2 processes..."
 pm2 stop all
 pm2 delete all
-pm2 save --force
+npm uninstall -g pm2
+rm -rf ~/.pm2
 
-echo "2. Dropping MongoDB database (jerry)..."
-mongosh jerry --eval "db.dropDatabase()"
-
-echo "3. Removing old project folder..."
-rm -rf /var/www/followerjerry.com
-
-echo "4. Removing old Nginx configs for followerjerry.com..."
-rm -f /etc/nginx/sites-available/followerjerry.com
-rm -f /etc/nginx/sites-enabled/followerjerry.com
-systemctl reload nginx
-
-echo "5. Removing Apache (if installed recently)..."
-systemctl stop apache2
-apt-get purge apache2 apache2-utils apache2-bin apache2.2-common -y
+echo "2. Completely removing MongoDB..."
+systemctl stop mongod
+apt-get purge -y mongodb-org*
 apt-get autoremove -y
+rm -r /var/log/mongodb
+rm -r /var/lib/mongodb
 
-echo ">>> Server cleaning complete! <<<"
+echo "3. Completely removing Node.js & NPM..."
+apt-get purge -y nodejs npm
+apt-get autoremove -y
+rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* ~/.npm
+rm -rf /usr/local/lib/node_modules
+rm -rf /etc/apt/sources.list.d/nodesource.list
+
+echo "4. Removing old Nginx..."
+systemctl stop nginx
+apt-get purge -y nginx nginx-common
+apt-get autoremove -y
+rm -rf /etc/nginx
+
+echo "5. Removing old Apache..."
+systemctl stop apache2
+apt-get purge -y apache2 apache2-utils apache2-bin apache2.2-common
+apt-get autoremove -y
+rm -rf /etc/apache2
+
+echo "6. Removing Certbot (SSL)..."
+apt-get purge -y certbot python3-certbot-nginx python3-certbot-apache
+apt-get autoremove -y
+rm -rf /etc/letsencrypt
+rm -rf /var/lib/letsencrypt
+
+echo "7. Deleting ALL project folders from /var/www/..."
+rm -rf /var/www/*
+
+echo "8. Cleaning up system packages..."
+apt-get autoremove -y
+apt-get clean
+
+echo ">>> Server is now completely factory reset. <<<"
